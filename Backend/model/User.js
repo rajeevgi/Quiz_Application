5ae35ecd79be66c7015f9bf2ac628696832fb797
rajeev <rajivg216@gmail.com>
2025-03-13 18:13:11 +0530

    Hashing Password Done

--- Backend/model/User.js
+++ Backend/model/User.js
@@ -24,10 +24,15 @@ const userSchema = mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function(next) {
-    if(!this.isModified('password')) return next();
-    this.password = bcrypt.hashSync(this.password, 10);
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next(); 
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate salt
+        this.password = await bcrypt.hash(this.password, salt); // Hash password
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
-
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- Backend/routes/Auth.js
+++ Backend/routes/Auth.js
@@ -1,32 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
-router.post('/register', async (req, res) => {
-    try {
-        const { name, email, password, role } = req.body;
-        const user =  new User({ name, email, password, role });
-        await user.save();
-        res.status(200).json({ message : "User Registered Successfully.."})
-    } catch (error) {
-        return res.status(500).json({ message : "Something went wrong!"});
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists!" });
     }
+    const user = new User({ name, email, password, role });
+
+    const savedUser = await user.save();
+
+    res
+      .status(200)
+      .json({ message: "User Registered Successfully..", savedUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if(!user || !bcrypt.compareSync(password, user.password)){
-            return res.status(401).json({ message : "Invalid Credentials" });
-        }
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_KEY, { expiresIn: '1h'});
-        res.json({ token });
-    } catch (error) {
-        return res.status(500).json({ message : "Login Error, Something went wrong!"});
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: "Invalid Credentials" });
     }
+    const token = jwt.sign({ id: user._id, role: user.role },process.env.JWT_KEY,{ expiresIn: "1h" });
+    // res.json({ token });
+    res.status(201).json({ message: "Login Successfully..", token });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Login Error, Something went wrong!" });
+  }
 });
 
 module.exports = router;
